fdf9ce6c530ea6011d0d6955f144e53052f46632
waijinchai <wcha0106@student.monash.edu>
2023-07-04 18:07:50 +0800

    Complete Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './Navbar'
+import Hero from "./Hero"
 import './App.css'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,14 @@
+import photoGrid from "./assets/photo-grid.png"
+import "./Hero.css"
+
+function Hero() {
+    return (
+        <section className="hero">
+            <img src= { photoGrid } alt="photo grid" className="hero--photo-grid"/>
+            <h1 className="hero--title">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
+
+export default Hero
